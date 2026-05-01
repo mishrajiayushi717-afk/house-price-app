e3526f647e1e5cbcb2ed6491fc2e9a63575efde2
mishrajiayushi717-afk <mishrajiayushi717@gmail.com>
2026-05-01 12:40:56 +0530

    my project1.py

--- project1.py
+++ project1.py
@@ -61,24 +61,24 @@ simple_input = np.array([[2000,3,2,1]])
 prediction = model.predict(simple_input)
 print('prediction Price:',prediction)
 
-# Commented out IPython magic to ensure Python compatibility.
-# %%writefile app.py
-# import pickle
-# import streamlit as st
-# import numpy as np
-# model = pickle.load(open('model.pkl', 'rb'))
-# #st.error("Model file not found. Please check the file path.")
-# st.title('House Price Prediction')
-# sqft = st.number_input("SqFt Area", min_value=0.0)
-# bed = st.number_input("Bedrooms", min_value=0.0)
-# bath = st.number_input("Bathrooms", min_value=0.0)
-# nb = st.number_input("Neighborhood", options = [0,1,],help="0 for Area A,1 for Area B")
-# 
-# if st.button("predict Price"):
-#   data = np.array([[sqft,bed,bath,nb]])
-#   prediction = model.predict(data)
-#   st.success('Predicted Price: {}'.format(prediction))
-#
+ Commented out IPython magic to ensure Python compatibility.
+ %%writefile app.py
+ import pickle
+ import streamlit as st
+ import numpy as np
+ model = pickle.load(open('model.pkl', 'rb'))
+ st.error("Model file not found. Please check the file path.")
+ st.title('House Price Prediction')
+ sqft = st.number_input("SqFt Area", min_value=0.0)
+ bed = st.number_input("Bedrooms", min_value=0.0)
+ bath = st.number_input("Bathrooms", min_value=0.0)
+ nb = st.number_input("Neighborhood", options = [0,1,],help="0 for Area A,1 for Area B")
+ 
+ if st.button("predict Price"):
+   data = np.array([[sqft,bed,bath,nb]])
+   prediction = model.predict(data)
+   st.success('Predicted Price: {}'.format(prediction))
+
 
 !pip install streamlit
 
